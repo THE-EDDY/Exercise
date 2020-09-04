43ba9f6d0296b5adfc7ba523b4044ab3a77c15b5
Leandro Albin <leoalbin@gmail.com>
2020-09-04 14:45:21 -0300

    Initial commit

---
+++ exercise-1/App/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App bg-gray-300 w-full h-screen mt-0 p-12">
+        <div className="max-w-sm w-full lg:max-w-full lg:flex">
+        <div className="bg-white flex flex-col justify-between leading-normal m-auto p-12">
+          <div className="mb-8">
+            <div className="text-gray-900 font-bold text-4xl mb-12">Welcome to the hiring exercise 1</div>
+            <p className="text-gray-700 text-base">Please follow the brief in README.md</p>
+          </div>
+        </div>
+      </div>
+  </div>
+  );
+}
+
+export default App;
